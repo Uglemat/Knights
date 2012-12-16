3102a41293711899b6e2c99ea31657eddcd6dfac
Smart Viking <smartviking@smartviking.(none)>
2012-12-16 05:59:17 +0100

    New arguments: --settings (list all settings) and --help (show arguments)

--- knights/args.py
+++ knights/args.py
@@ -36,6 +36,12 @@ else: # Python 3, or python 4 if you're from the distant future
 """
 
 
+def print_setting(n, setting, default):
+    print("{indent}{setting:<{width}}VALUE: {def_val}".format(indent=" "*n,
+                                                                setting=setting,
+                                                                def_val=default,
+                                                                width=40-n))
+
 def do_stuff_with_args():
     exit_after = False
     for arg in sys.argv[1:]:
@@ -43,6 +49,27 @@ def do_stuff_with_args():
         if not match:
             if arg in ["--version","-v"]:
                 print("Knights version: {0}".format(VERSION))
+                exit_after = True
+            elif arg in ["--settings","-s"]:
+                for key in settings:
+                    val = settings[key]
+                    if isinstance(val, dict):
+                        print("{0}:".format(key))
+                        for subsetting in val:
+                            print_setting(4,subsetting,val[subsetting])
+                        print("")
+                    else:
+                        print_setting(0,key,val)
+                        print("")
+                exit_after = True
+            elif arg in ["-help","--help","help","-h","?","--h"]:
+                arg_str = lambda s: "{0:<15}".format(s)
+                print("Arguments:")
+                print(arg_str("--version, -v"), "Print game version")
+                print(arg_str("--settings, -s"), "List all possible settings")
+                print(arg_str("--help, -s"), "List arguments")
+                print(arg_str("setting=value"),"Set some setting to some value. To access nested settings,")
+                print(arg_str(""),"use dot notation. value can be enclosed in strings.")
                 exit_after = True
             else:
                 print("Argument makes non sense: {0}".format(arg))
